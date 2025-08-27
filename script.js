d7a667a4f8dc9bab763ed8c1681c1833177f7ece
MD. Noruzzaman <tonmoyakand8@gmail.com>
2025-08-27 18:46:01 +0600

    create a login page

---
+++ script.js
@@ -0,0 +1,7 @@
+// console.log('Script connected')
+//login button functionality
+document.getElementById("loginButton").addEventListener('click',function(e){ 
+    e.preventDefault()
+    console.log('login button click')
+    console.log(e)
+})
