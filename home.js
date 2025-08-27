b8a5f35064b2ab9c739960a667e9167b8a655f40
MD. Noruzzaman <tonmoyakand8@gmail.com>
2025-08-27 23:56:03 +0600

    added a add money form section

--- home.js
+++ home.js
@@ -1,1 +1,32 @@
-console.log("connect home file")
+// console.log("connect home file")
+
+const validPin = 1234
+
+document.getElementById("add-money-btn").addEventListener("click",function(e){
+    e.preventDefault()
+    // console.log("add money btn clicked")
+
+    const bank = document.getElementById("bank").value
+    const accountNumber = document.getElementById("account-number").value 
+    const amount = parseInt(document.getElementById("add-amount").value)
+
+    const pin = parseInt(document.getElementById("add-pin").value)
+    // console.log(bank, accountNumber, amount, pin)
+    const availableBalance = parseInt(document.getElementById("available-balance").innerText)
+    // console.log(availableBalance)
+
+    if(accountNumber.length <11){
+
+        alert("please provide 11 digit valid account number")
+        return;
+    }
+
+    if(pin != validPin){
+        alert("please provide valid pin number")
+        return;
+    }
+
+    const totalNewBalance = amount+availableBalance
+
+    document.getElementById("available-balance").innerText = totalNewBalance
+})
