d1fa03a4587067f078da185cbde9108fa09d8514
MD. Noruzzaman <tonmoyakand8@gmail.com>
2025-08-27 19:07:31 +0600

    connect home page with login page

--- script.js
+++ script.js
@@ -2,6 +2,21 @@
 //login button functionality
 document.getElementById("loginButton").addEventListener('click',function(e){ 
     e.preventDefault()
-    console.log('login button click')
-    console.log(e)
+    const mobileNumber = 12345678910
+    const pinNumber = 1234
+    const mobileNumberValue = document.getElementById("mobile-number").value
+    const mobileNumberValueConverted = parseInt(mobileNumberValue)
+
+    const pinNumberValue = document.getElementById("pin-number").value
+    const pinNumberConverted = parseInt(pinNumberValue)
+
+    console.log(mobileNumberValueConverted, pinNumberConverted)
+
+    if(mobileNumberValueConverted === mobileNumber && pinNumberConverted === pinNumber){
+        window.location.href="./home.html"
+    }
+    else{
+        alert("Invalid credentials")
+    }
+
 })
